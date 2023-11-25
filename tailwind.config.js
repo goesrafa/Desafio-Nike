c57ee715bd8de2509c8442fc31bf1af61f2f34e4
goesrafa <goes.rafaelasilva@gmail.com>
2023-11-25 19:05:37 -0300

    Finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+       "sans":["Poppins", "sans-serif"]
+      },
+      backgroundImage:{
+        "tenis": "url('/src/assets/bg.png')"
+      },
+      keyframes:{
+        floatRight:{
+          "0%":{  transform: "translatex(0px)"},
+          "50%":{  transform: "translatex(8px)"},
+          "100%":{  transform: "translatex(0px)"},
+        },
+        
+      },
+      animation:{
+        floatRight: "floatRight 2.5s infinite",
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
